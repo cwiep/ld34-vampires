0f274ab9033e386998271d765b367f6ece743797
Christian Wieprecht <christian.wieprecht@udo.edu>
2015-12-14 18:27:34 +0100

    Using correct canvas size in html launcher.

--- html/src/de/cwiep/vampires/client/HtmlLauncher.java
+++ html/src/de/cwiep/vampires/client/HtmlLauncher.java
@@ -5,12 +5,13 @@ import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 
 import de.cwiep.vampires.GameController;
+import de.cwiep.vampires.GameRulesConstants;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(GameRulesConstants.V_WIDTH, GameRulesConstants.V_HEIGHT);
         }
 
         @Override
